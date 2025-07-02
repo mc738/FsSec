b67415de34359a9dcfdd3026f79f605e54ed9655
Max <me@mclifford.dev>
2025-07-02 23:32:43 +0100

    chore(core): Added Impl.fs

---
+++ FsSec/V1/Store/SQLite/Impl.fs
@@ -0,0 +1,17 @@
+﻿namespace FsSec.V1.Store.SQLite
+
+open System
+open Freql.Sqlite
+open FsSec.V1.Store.Core
+open FsToolbox.Core.Results
+
+[<AutoOpen>]
+module Impl =
+
+    type SQLiteFsSecStore(ctx: SqliteContext) =
+
+        interface IFsSecStore with
+
+            member this.Initialize() = Operations.initialize ctx
+
+            member this.Dispose() = (ctx :> IDisposable).Dispose()
