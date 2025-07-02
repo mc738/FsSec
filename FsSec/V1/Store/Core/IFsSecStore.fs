bdbfe4d0d553400faff3b57337d3f2bbdd225847
Max <me@mclifford.dev>
2025-07-02 23:33:30 +0100

    chore(core): Added IFsSecStore.fs

---
+++ FsSec/V1/Store/Core/IFsSecStore.fs
@@ -0,0 +1,11 @@
+﻿module FsSec.V1.Store.Core
+
+open System
+open FsToolbox.Core.Results
+
+type IFsSecStore =
+    
+    inherit IDisposable
+    
+    abstract member Initialize: unit -> ActionResult<unit>
+    
