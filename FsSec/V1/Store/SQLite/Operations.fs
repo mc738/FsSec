948a0afadcb06f411b379affb3422cd13416d18e
Max <me@mclifford.dev>
2025-07-02 23:33:01 +0100

    chore(core): Added Operations.fs

---
+++ FsSec/V1/Store/SQLite/Operations.fs
@@ -0,0 +1,17 @@
+﻿namespace FsSec.V1.Store.SQLite
+
+open Freql.Sqlite
+open FsToolbox.Core.Results
+
+module Operations =
+
+    let initialize (ctx: SqliteContext) =
+        try
+            ActionResult.Success()
+        with exn ->
+            ActionResult.Failure
+                { Message = $"Failed to initialize SQLiteFsSecStore. Message: {exn.Message}"
+                  DisplayMessage = "Failed to initialize SQLiteFsSecStore"
+                  Exception = Some exn
+                  IsTransient = false
+                  Metadata = Map.empty }
