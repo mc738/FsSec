ccf52df85454aa62b8d9587a586601359c85c3e4
Max <me@mclifford.dev>
2025-07-05 23:12:23 +0100

    chore(core): Added Configuration.fs

---
+++ FsSec/V1/Core/Configuration.fs
@@ -0,0 +1,19 @@
+﻿namespace FsSec.V1.Core
+
+open System
+open FsToolbox.Extensions.Strings
+
+module Configuration =
+
+
+    type ConfigurationValueType =
+        | Literal of string
+        | EnvironmentalVariable of string
+
+    let tryGetEnvironmentalVariable (name: string) =
+        match name.IsNullOrWhiteSpace() with
+        | true -> None
+        | false ->
+            match name.[0] with
+            | '#' -> Environment.GetEnvironmentVariable(name.Substring(1)).ToOption()
+            | _ -> Environment.GetEnvironmentVariable(name).ToOption()
