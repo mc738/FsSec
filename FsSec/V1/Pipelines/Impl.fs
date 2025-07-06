9f628f12e3334a3c9a9216774bce0508f495d69e
Max <me@mclifford.dev>
2025-07-06 22:45:39 +0100

    chore(core): Work on Impl.fs

--- FsSec/V1/Pipelines/Impl.fs
+++ FsSec/V1/Pipelines/Impl.fs
@@ -1,64 +1,49 @@
 ﻿namespace FsSec.V1.Pipelines
 
 open System
+open FsSec.V1.Pipelines.Configuration
+open FsSec.V1.Store.Impl
 open FsToolbox.Core.Results
 
 type Pipeline =
-        { Context: PipelineContext
-          Steps: PipelineStep list }
+    { Context: PipelineContext
+      Steps: PipelineStep list }
 
-        interface IDisposable with
-            member this.Dispose() = this.Context.Store.Dispose()
-
-        member p.Initialize() =
-            p.Context.Store.Initialize()
-            |> ActionResult.bind (fun _ ->
-
-                p.Steps
-                |> List.fold
-                    (fun (result: PipelineStepInitializationResult) step ->
-                        match result with
-                        | PipelineStepInitializationResult.Success -> step.Handler.Initialize(p.Context)
-                        | PipelineStepInitializationResult.Skipped _ ->
-                            match step.Handler.Initialize(p.Context) with
-                            | PipelineStepInitializationResult.Success as r -> r
-                            | PipelineStepInitializationResult.Skipped _ as r -> r
-                            | PipelineStepInitializationResult.Failure _ as r when step.Mandatory -> r
-                            | _ -> PipelineStepInitializationResult.Skipped "Non-mandatory step failed"
-                        | PipelineStepInitializationResult.Failure _ -> result)
-                    PipelineStepInitializationResult.Success
-                |> function
-                    | PipelineStepInitializationResult.Success
-                    | PipelineStepInitializationResult.Skipped _ -> ActionResult.Success()
-                    | PipelineStepInitializationResult.Failure failureResult -> ActionResult.Failure failureResult)
-
-        member p.Run(?skipInitialization: bool) =
+    interface IDisposable with
+        member this.Dispose() = this.Context.Store.Dispose()
 
 
-            // Initialize first
-            if skipInitialization |> Option.defaultValue false then
-                ActionResult.Success()
-            else
-                ActionResult.Success()
-            |> ActionResult.bind (fun _ ->
-                // Then run set up
+    static member TryCreate(cfg: PipelineConfiguration) =
+        // First try and create the context.
+        try
+            let runId = Guid.NewGuid().ToString("n")
 
-                ActionResult.Success())
-            |> ActionResult.bind (fun _ ->
-                // Then execute
+            let store = getStore cfg.Store.StoreType
 
-                ActionResult.Success())
-            |> ActionResult.bind (fun _ ->
-                // Then clean up
-
-                ActionResult.Success())
-            |> function
-                | ActionResult.Success _ -> ()
-                | ActionResult.Failure failureResult -> failwith "todo"
+            let ctx = PipelineContext.Create(runId, "", store)
 
 
+            Ok(
+                { Context = PipelineContext.Create(runId, "", store)
+                  Steps = failwith "" }
+            )
+        with ex ->
+            Error $"Error while creating pipeline. Message: {ex.Message}"
 
+    member p.Initialize() =
+        Operations.Initialization.run p.Context p.Steps
 
-
-            ()
-    
+    member p.Run(?skipInitialization: bool) =
+        // Initialize first
+        if skipInitialization |> Option.defaultValue false then
+            ActionResult.Success()
+        else
+            ActionResult.Success()
+        |> ActionResult.bind (fun _ -> Operations.StepUp.run p.Context p.Steps)
+        |> ActionResult.bind (fun _ -> Operations.Preprocessing.run p.Context p.Steps)
+        |> ActionResult.bind (fun _ -> Operations.Execution.run p.Context p.Steps)
+        |> ActionResult.bind (fun _ -> Operations.PostProcessing.run p.Context p.Steps)
+        |> ActionResult.bind (fun _ -> Operations.CleanUp.run p.Context p.Steps)
+        |> function
+            | ActionResult.Success _ -> ()
+            | ActionResult.Failure failureResult -> failwith "todo"
