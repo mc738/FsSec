0e9aa0881bc07920ba0b63f9b5437b8dd7d50c26
Max <me@mclifford.dev>
2025-07-06 22:45:27 +0100

    chore(core): Work on Configuration.fs

--- FsSec/V1/Pipelines/Configuration.fs
+++ FsSec/V1/Pipelines/Configuration.fs
@@ -1,2 +1,27 @@
-﻿namespace FsSec.V1.Pipelines.Configuration
+﻿namespace FsSec.V1.Pipelines
 
+open System
+open FsSec.V1.Store.Configuration
+open FsToolbox.Extensions.Strings
+open FsSec.V1.Core.Configuration
+
+module Configuration =
+
+    let retResolveConfigurationValue (ctx: PipelineContext) (value: ConfigurationValueType) =
+        match value with
+        | ConfigurationValueType.Literal s -> s.ToOption()
+        | ConfigurationValueType.EnvironmentalVariable s -> Environment.GetEnvironmentVariable(s).ToOption()
+        | ConfigurationValueType.Variable s -> ctx.TryGetVariable s
+        | ConfigurationValueType.Path s -> ctx.TryResolvePath s
+
+
+    type PipelineConfiguration = { Store: PipelineStoreConfiguration }
+
+    and PipelineStoreConfiguration = { StoreType: StoreType }
+
+    and PipelineStepConfiguration =
+        {
+            Name: string
+            Mandatory: string
+            StepType: string
+        }
