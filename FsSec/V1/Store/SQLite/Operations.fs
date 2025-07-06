31c781b8767700f21428651acb271cadc9fb1a99
Max <me@mclifford.dev>
2025-07-06 22:46:10 +0100

    chore(core): Work on Operations.fs

--- FsSec/V1/Store/SQLite/Operations.fs
+++ FsSec/V1/Store/SQLite/Operations.fs
@@ -2,12 +2,21 @@
 
 open Freql.Sqlite
 open FsToolbox.Core.Results
+open FunkyPM.V1.Store.SQLite.Persistence
 
 module Operations =
 
     let initialize (ctx: SqliteContext) =
         try
-            ActionResult.Success()
+            match Initialization.runInTransaction true ctx with
+            | Ok _ -> ActionResult.Success()
+            | Error errorValue ->
+                ActionResult.Failure
+                    { Message = errorValue.Message
+                      DisplayMessage = "Initialize failed"
+                      Exception = errorValue.Exception
+                      IsTransient = false
+                      Metadata = Map.empty }
         with exn ->
             ActionResult.Failure
                 { Message = $"Failed to initialize SQLiteFsSecStore. Message: {exn.Message}"
