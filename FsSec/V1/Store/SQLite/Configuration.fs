e4a2a0f1ab769b273938a1d5a0e8f5bf13d74cd4
Max <me@mclifford.dev>
2025-07-07 22:10:37 +0100

    chore(core): Added Configuration.fs

---
+++ FsSec/V1/Store/SQLite/Configuration.fs
@@ -0,0 +1,15 @@
+﻿namespace FsSec.V1.Store.SQLite
+
+open System.Text.Json
+open FsSec.V1.Core.Configuration
+open FsToolbox.Core
+
+module Configuration =
+
+    type SQLiteFsSecStoreConfiguration =
+        { Path: ConfigurationValueType }
+
+        static member TryFromJson(element: JsonElement) =
+            match Json.tryGetStringProperty "path" element with
+            | None -> Error "Missing `path` property"
+            | Some value -> { Path = ConfigurationValueType.Deserialize value } |> Ok
