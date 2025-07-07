f7317f50947b02e36df1a31a358d77cad49d2d93
Max <me@mclifford.dev>
2025-07-07 22:11:03 +0100

    chore(core): Added Domain.fs

---
+++ FsSec/V1/Store/Core/Domain.fs
@@ -0,0 +1,11 @@
+﻿namespace FsSec.V1.Store.Core
+
+module Domain =
+
+    type Version =
+        | Latest
+        | Specific of int
+
+    type EntityId =
+        | Generated
+        | Specific of string
