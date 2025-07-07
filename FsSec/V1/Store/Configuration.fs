2f2c2ac0a2e29f4dbcc0de36f6715e9efcd8f772
Max <me@mclifford.dev>
2025-07-07 22:10:47 +0100

    chore(core): Added Configuration.fs

---
+++ FsSec/V1/Store/Configuration.fs
@@ -0,0 +1,18 @@
+﻿namespace FsSec.V1.Store
+
+open System.Text.Json
+open FsSec.V1.Store.SQLite.Configuration
+open FsToolbox.Core
+
+module Configuration =
+
+    type StoreType =
+        | SQLite of SQLiteFsSecStoreConfiguration
+
+        static member TryFromJson(element: JsonElement) =
+            match Json.tryGetStringProperty "type" element with
+            | None -> Error "Missing `type` property"
+            | Some value ->
+                match value.ToLower() with
+                | "sqlite" -> SQLiteFsSecStoreConfiguration.TryFromJson element |> Result.map StoreType.SQLite
+                | _ -> Error $"Unknown store type: `{value}`"
