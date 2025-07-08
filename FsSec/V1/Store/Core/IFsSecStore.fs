a037c4724434fe15391985a8cc1e600d72d48dc5
Max <me@mclifford.dev>
2025-07-08 12:15:08 +0100

    chore(core): Initial impl

--- FsSec/V1/Store/Core/IFsSecStore.fs
+++ FsSec/V1/Store/Core/IFsSecStore.fs
@@ -1,6 +1,8 @@
 ﻿module FsSec.V1.Store.Core
 
 open System
+open FsSec.V1.Core.Artifacts
+open FsSec.V1.Core.Resources
 open FsToolbox.Core.Results
 
 type IFsSecStore =
@@ -8,4 +10,30 @@ type IFsSecStore =
     inherit IDisposable
     
     abstract member Initialize: unit -> ActionResult<unit>
+    
+    abstract member InitializePipelineRun: RunId: string -> ActionResult<unit>
+    
+    abstract member AddPipelineStepResult: RunId: string -> ActionResult<unit>
+    
+    abstract member GetGlobalKeyValue: Key: string -> FetchResult<string option>
+    
+    abstract member GetKeyValue: RunId: string * Key: string -> FetchResult<string option>
+    
+    abstract member AddOrUpdateKeyValue: RunId: string * Key: string * Value: string -> ActionResult<unit>
+    
+    abstract member AddArtifact: RunId: string * Artifact: Artifact * Version: Version -> ActionResult<unit>
+    
+    abstract member GetArtifact: RunId: string * ArtifactId: string * Version: Version -> FetchResult<Artifact option>
+   
+    abstract member AddGlobalArtifact: Artifact: Artifact * Version: Version -> ActionResult<unit>
+    
+    abstract member GetGlobalArtifact: ArtifactId: string * Version: Version -> FetchResult<Artifact option>
+    
+    abstract member AddResource: RunId: string * Resource: Resource * Version: Version -> ActionResult<unit>
+    
+    abstract member GetResource: RunId: string * ResourceId: string * Version: Version -> FetchResult<Resource option>
+    
+    abstract member AddGlobalResource: Resource: Resource * Version: Version -> ActionResult<unit>
+    
+    abstract member GetGlobalResource: ResourceId: string * Version: Version -> FetchResult<Resource option>
     
