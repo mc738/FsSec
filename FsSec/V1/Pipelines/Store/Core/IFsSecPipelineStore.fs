3831411381d14560be922f8173d7a7f18d9e9a3a
Max <me@mclifford.dev>
2025-07-08 12:15:02 +0100

    chore(core): Initial impl

---
+++ FsSec/V1/Pipelines/Store/Core/IFsSecPipelineStore.fs
@@ -0,0 +1,28 @@
+﻿namespace FsSec.V1.Pipelines.Store.Core
+
+open FsSec.V1.Core.Artifacts
+open FsSec.V1.Core.Resources
+open FsToolbox.Core.Results
+
+type IFsSecPipelineStore =
+    
+    abstract member Initialize: unit -> ActionResult<unit>
+    
+    abstract member InitializePipelineRun: RunId: string -> ActionResult<unit>
+    
+    abstract member AddPipelineStepResult: RunId: string -> ActionResult<unit>
+    
+    abstract member GetGlobalKeyValue: Key: string -> FetchResult<string option>
+    
+    abstract member GetKeyValue: RunId: string * Key: string -> FetchResult<string option>
+    
+    abstract member AddOrUpdateKeyValue: RunId: string * Key: string * Value: string -> ActionResult<unit>
+    
+    abstract member AddArtifact: RunId: string * Artifact: Artifact * Version: Version -> ActionResult<unit>
+    
+    abstract member GetArtifact: RunId: string * ArtifactId: string * Version: Version -> FetchResult<Artifact option>
+    
+    abstract member AddResource: RunId: string * Resource: Resource * Version: Version -> ActionResult<unit>
+    
+    abstract member GetResource: RunId: string * ResourceId: string * Version: Version -> FetchResult<Resource option>
+    
