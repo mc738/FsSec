3622fc89e1cf31dd732c40c1750de673ee702f2a
Max <me@mclifford.dev>
2025-07-09 12:15:38 +0100

    chore(core): Initial impl

--- FsSec.App/Program.fs
+++ FsSec.App/Program.fs
@@ -2,15 +2,42 @@
 open System.IO
 open System.Net
 open System.Net.Http
+open FsSec.V1.Core.Configuration
+open FsSec.V1.Pipelines
+open FsSec.V1.Pipelines.Configuration
+open FsSec.V1.Store.Configuration
+
+
+module I =
+
+    let logo =
+        [ "  ____|       ___|             "
+          "  |     __| \___ \    _ \   __|"
+          "  __| \__ \       |   __/  (   "
+          " _|   ____/ _____/  \___| \___|"
+          ""
+          "FsSec version 1"
+          "" ]
+
+    let printLogo () = logo |> List.iter (printfn "%s")
+
+module Test =
+
+    let cfg =
+        ({ Store =
+            ({ StoreType = StoreType.SQLite { Path = ConfigurationValueType.Literal "" } }: PipelineStoreConfiguration) }
+        : PipelineConfiguration)
+
+    let create (cfg: PipelineConfiguration) =
+        match Pipeline.TryCreate(cfg) with
+        | Error errorValue -> failwith "todo"
+        | Ok pipeline -> pipeline
 
 let test _ =
 
     let baseUrl = "http://localhost:8808/"
 
-    let urls =
-        [ "login.php"
-          "login"
-          "" ]
+    let urls = [ "login.php"; "login"; "" ]
 
     use client = new HttpClient()
 
@@ -38,10 +65,12 @@ let test _ =
 
                 return ()
         }
-        |> fun computation ->
-            Async.RunSynchronously computation)
+        |> fun computation -> Async.RunSynchronously computation)
 
     ()
+
+
+
 
 
 test ()
